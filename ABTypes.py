20541ed6f41241d666d84ba0543634b090b87cbf
d.m.korzhenkov <d.m.korzhenkov@tinkoff.ru>
2017-06-07 11:14:37 +0300

    type hints for ABTypes added

--- ABTypes.py
+++ ABTypes.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 import warnings
+from typing import List, Optional, Sequence, TypeVar
+
+PandasDataFrame = TypeVar('pandas.core.frame.DataFrame')
+Ndarray = TypeVar('ndarray')
 
 
 class Variation:
     """
     Class handles results provided with A/B test for a particular variation
     """
-    def __init__(self, total=None, success=None, data=None, group=None):
+    def __init__(self, total: Optional[int]=None, success: Optional[int]=None,
+                 data: Optional[Sequence[int]]=None, group: Optional[int]=None
+                 ) -> None:
         """
         Create Variation instance
         
-        :param int total: number of total visitors
-        :param int success: number of visitors who clicked the button
-        :param iterable data: sequence of visitors actions (1 - clicked, 0 - didn't click)
-        :param int group: size of group in data (for Wald tests) 
+        :param total: number of total visitors
+        :param success: number of visitors who clicked the button
+        :param data: sequence of visitors actions (1 - clicked, 0 - didn't click)
+        :param group: size of group in data (for Wald tests) 
         """
-        self.group = group
+
+        self.total = None  # type: Optional[int]
+        self.success = None  # type: Optional[int]
+        self.data = None  # type: List[int]
+        self.group = group  # type: Optional[int]
 
         if data is not None:
             try:
@@ -38,7 +48,7 @@ class Variation:
         if (self.total is not None) and (self.success is not None) and (self.total < self.success):
             raise Exception('Conversion rate > 1')
 
-    def estimate_conversion(self):
+    def estimate_conversion(self) -> float:
         """
         Estimate the sample conversion
         
@@ -49,19 +59,17 @@ class Variation:
         except:
             raise ZeroDivisionError()
 
-    def copy(self):
+    def copy(self) -> 'Variation':
         """
         Copy Variation instance
-        
-        :return: Variation
         """
         return Variation(self.total, self.success, self.data)
 
-    def truncate(self, size):
+    def truncate(self, size: int) -> 'Variation':
         """
         Truncate variation data size to proposed if it is less than current one.
         
-        :param int size: new size
+        :param size: new size
         :return: variation
         """
         if not self.data:
@@ -69,11 +77,9 @@ class Variation:
         else:
             return Variation(data=self.data[:1 + min(size, self.total)])
 
-    def generate_sample(self):
+    def generate_sample(self) -> Ndarray:
         """
         Generate a sample based on total number and number of successes.
-        
-        :return numpy.array:
         """
         import numpy as np
         mask = np.random.choice(self.total, self.success, replace=False)
@@ -87,29 +93,27 @@ class VariationsCollection:
     Set of variations obtained within A/B test.
     Includes one control variations and several experimental ones.
     """
-    def __init__(self, *args):
+    def __init__(self, *args: Variation) -> None:
         """
         Create VariationsCollection instance.
         The first argument is treated as a control variation, the other - as experimental ones.
         If no arguments are provided the empty instance would be created.
-        
-        :param Variation args:
         """
-        self.control = None
-        self.treatments = list()
+        self.control = None  # type: Optional[Variation]
+        self.treatments = list()  # type: Optional[List[Variation]]
 
         if len(args):
             self.add_control(args[0])
             if len(args) > 1:
                 self.add_treatments(*args[1:])
 
-    def add_control(self, variation):
+    def add_control(self, variation: Variation) -> None:
         """
         Add control variation to the collection.
         If control variation is already present it would be substituted and the warning would be printed to console.
         It's an in-place method.
         
-        :param Variation variation: new control variation
+        :param variation: new control variation
         """
         if not isinstance(variation, Variation):
             raise TypeError('Variation object only suits.')
@@ -117,12 +121,12 @@ class VariationsCollection:
             warnings.warn('Collection already has a control variation. It would be changed to proposed one.')
         self.control = variation
 
-    def add_treatments(self, *args):
+    def add_treatments(self, *args: Variation) -> None:
         """
         Add new treatment variations. Treatments are added to the end of experimental variations list.
         It'a an in-place method.
         
-        :param Variation args: experimental variations
+        :param args: experimental variations
         """
         if not len(args):
             warnings.warn('Nothing to add')
@@ -132,30 +136,28 @@ class VariationsCollection:
                     raise TypeError('Objects of Variation type only suit.')
             self.treatments += list(args)
 
-    def delete_control(self):
+    def delete_control(self) -> None:
         """
         Delete control variation.
         """
         self.control = None
 
-    def delete_treatment(self, n):
+    def delete_treatment(self, n: int) -> None:
         """
         Delete treatment with given number (counting from 1) if it exists.
         It's an in-place method.
         
-        :param int n: number of variation to be deleted.
+        :param n: number of variation to be deleted.
         """
         if n > len(self.treatments):
             warnings.warn('Number of experimental variations is less then proposed number.')
         else:
             del self.treatments[n-1]
 
-    def describe(self):
+    def describe(self) -> Optional[PandasDataFrame]:
         """
         Return summary information about collection.
-        If collection is empty returns None.
-        
-        :return pandas.DataFrame:  
+        If collection is empty returns None. 
         """
         import pandas as pd
         if (self.control is None) and (not len(self.treatments)):
